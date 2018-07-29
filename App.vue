2f7889da7f39e82b9b03043014d1822fbc7de7f8
daiyanze <y.dai@jxpress.net>
2018-07-29 23:38:26 +0900

    :sparkles: Working on typescript

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: 'app',
+  data () {
+    return {
+      message: 'Vue Keymap is here!'
+    }
+  }
+}
+</script>
+
