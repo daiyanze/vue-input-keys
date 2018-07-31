c18fd45b2366a1e470c134fef3ede2ac75e388c2
daiyanze <ginoalex8964@yahoo.com>
2018-07-31 14:29:39 +0900

    :sparkles: updated package.json scripts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { Vue as _Vue } from "./vue"
+import * as _Vue from "vue"
 
 export type PluginFunction<T> = (Vue: typeof _Vue, options?: T) => void;
 
 export interface PluginObject<T> {
 	  install: PluginFunction<T>
-		  [key: string]: any
+		[key: string]: any
 }
 
-class Plugin implements PluginObject {
+class Plugin implements PluginObject<any> {
   constructor () {
     console.log('it workds')
   }
+
+	public install (Vue: typeof _Vue, options?: Object): void {
+		console.log('installing vue plugin')
+	}
 }
